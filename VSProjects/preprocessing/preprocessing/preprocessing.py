0d7b425efea0081e8ca28813fe14ba52ec47a0e6
masa865 <sirimochimochi@gmail.com>
2020-10-26 17:18:09 +0900

    preprocessing

--- VSProjects/preprocessing/preprocessing/preprocessing.py
+++ VSProjects/preprocessing/preprocessing/preprocessing.py
@@ -4,16 +4,18 @@ import cv2
 def preprocess(img_bgr):
     #extract V of HSV space
     img_v = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2GRAY)
+    img_prep = img_v
     
-    return img_v
+    return img_prep
 
-#test code
+
+#test code for this function
 if __name__ == '__main__':
     img = cv2.imread(r"C:\Users\sirim\Pictures\new\deru.jpg")
-    prepro_img = preprocess(img)
+    img_prep = preprocess(img)
     print("shape")
-    print(prepro_img.shape)
+    print(img_prep.shape)
     print("data type")
-    print(prepro_img.dtype)
-    cv2.imshow('image',prepro_img)
+    print(img_prep.dtype)
+    cv2.imshow('image',img_prep)
     cv2.waitKey(0)
