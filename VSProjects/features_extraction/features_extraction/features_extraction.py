b7a391622f301a854e2514f646b7bbc20412336c
masa865 <sirimochimochi@gmail.com>
2020-11-12 19:19:18 +0900

    features_extraction

--- VSProjects/features_extraction/features_extraction/features_extraction.py
+++ VSProjects/features_extraction/features_extraction/features_extraction.py
@@ -1,5 +1,9 @@
 #for Features extraction of wood
+import tensorflow as tf
+from tensorflow import keras
+
 import cv2
+
 import numpy as np
 
 #calculate parameter of line equation from 2 point
@@ -18,10 +22,10 @@ def getCircleXY(radius,center=(0,0),divn=10):
 
     theta = np.linspace(0,2*np.pi,divn) #radian
 
-    x = center[0] + radius * np.cos(theta)
-    y = center[1] + radius * np.sin(theta)
+    X = center[0] + radius * np.cos(theta) #X is ndarray of x coordinate
+    Y = center[1] + radius * np.sin(theta) #Y is ndarray of y coordinate
 
-    return x,y
+    return X,Y
 
 #calculate num of annual rings
 def getRingNum(outerX,outerY,center=(0,0)):
@@ -43,13 +47,47 @@ def obtainEdges(img_gray,minVal=100,maxVal=200,filter_size=3):
 
     return img_edge
 
+def splitImg(img,split_size=64):
+
+    v_size = img.shape[0] // size * size
+    h_size = img.shape[1] // size * size
+    img = img[:v_size, :h_size]
+
+    v_split = img.shape[0] // size #split num of vertical direction
+    h_split = img.shape[1] // size #split num of horizontal direction
+
+    i=0
+    j=0
+    for h_img in np.vsplit(img, v_split):
+        for v_img in np.hsplit(h_img, h_split):
+                #cv2.imwrite(r"C:\Users\user\Pictures\opencv_test_pic\test" + r"\img_%03.f"%(i) + "_%03.f"%(j) + ".jpg",v_img)
+                cv2.imwrite(r"C:\Users\sirim\Pictures\new\output\test_image" + r"\img_%03.f_%03.f"%(i,j) + ".tif",v_img)
+                j+=1 #update row index
+        j=0 #reset row index
+        i+=1 #update col index
+
+    return splited_imgs,splited_index
+
 #main function of this module
-def extractFeature(img,centerX,senterY,radius,classList=None):
+def extractFeature(img,x_center,y_center,radius,model):
+    #img:v channel of hsv
 
-    #obtain edge
+    #select Low-noise line
+    #split img
+    split_size = 64
+
+    
+    #predict
+    predictions = model.predict(splited_imgs)
+    predict_results = predictions.argmax(axis=1) #result list of classification
+
+    #create flag img
+    flag_img = numpy.zeros_like(img)
+    class_dict = {"high":0,"low":1}
+    
+
+    #obtain edge image
     img_edge = obtainEdges(img)
-
-    #select Low-noise line 
 
     #calculate NR
 
