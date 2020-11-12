7685da8ae6a1a7f3536201bb22bcd5e372fc0914
masa865 <sirimochimochi@gmail.com>
2020-11-12 22:41:56 +0900

    features_extraction

--- VSProjects/features_extraction/features_extraction/features_extraction.py
+++ VSProjects/features_extraction/features_extraction/features_extraction.py
@@ -5,17 +5,44 @@ from tensorflow import keras
 import cv2
 
 import numpy as np
+import math
 
-#calculate parameter of line equation from 2 point
-def lineEquation(x0,y0,xn,yn):
+#split image
+def splitImg(img,split_size=64):
 
-    if xn - x0 == 0: #if intersept = infinity 
-        return (float('inf'), None)
+    v_size = img.shape[0] // split_size * split_size
+    h_size = img.shape[1] // split_size * split_size
+    img = img[:v_size, :h_size]
+
+    v_split = img.shape[0] // split_size #split num of vertical direction
+    h_split = img.shape[1] // split_size #split num of horizontal direction
+
+    splited_imgs = []
+    i=0
+    j=0
+    for h_img in np.vsplit(img, v_split):
+        for v_img in np.hsplit(h_img, h_split):
+                splited_imgs.append(v_img)
+                j+=1 #update horizontal direction index
+        j=0 #reset horizontal direction index
+        i+=1 #update vertical direction index
     
-    intersept = (y0 - yn) / (x0 - xn)
-    slope = y0 - intersept * x0
 
-    return intersept,slope
+    return np.array(splited_imgs),v_split,h_split
+
+#create flag image
+def createFlag(img,v_split,h_split,predict_results,split_size=64):
+
+    flag_img = np.zeros_like(img)
+    k = 0
+    for i in range(v_split):
+        for j in range(h_split):
+            if(predict_results[j+k]):
+                flag_img[i*split_size:i*split_size+split_size,
+                         j*split_size:j*split_size+split_size] = 1
+        k+=h_split
+
+    return flag_img
 
 #calculate coordinate of circle
 def getCircleXY(radius,center=(0,0),divn=10):
@@ -25,71 +52,75 @@ def getCircleXY(radius,center=(0,0),divn=10):
     X = center[0] + radius * np.cos(theta) #X is ndarray of x coordinate
     Y = center[1] + radius * np.sin(theta) #Y is ndarray of y coordinate
 
-    return X,Y
-
-#calculate num of annual rings
-def getRingNum(outerX,outerY,center=(0,0)):
-
-    x0,y0 = center
-
-    outerX_n = outerX.size
-    for i in outerX_n:
-        intersept = (y0 - outerX[i]) / (x0 - outerY[i])
-        num = 0
-
-
-
+    return (X,Y)
 
 #obtain edges of the annual rings
-def obtainEdges(img_gray,minVal=100,maxVal=200,filter_size=3):
+def obtainEdges(img,minVal=100,maxVal=200,filter_size=3):
 
-    img_edge = cv2.Canny(img_gray,minVal,maxVal,filter_size)
+    img_edge = cv2.Canny(img,minVal,maxVal,filter_size)
 
     return img_edge
 
-def splitImg(img,split_size=64):
+def getNR(img,center_x,center_y,outerX,outerY):
+    #img:edge image
 
-    v_size = img.shape[0] // size * size
-    h_size = img.shape[1] // size * size
-    img = img[:v_size, :h_size]
+    ring_nums = np.zeros_like(outerX)
+    line_index = 0
+    for outerx,outery in outerX,outerY:
+        intersept = (center_y - outery) / (center_x - outerx)
+        x = math.ceil(outerx)
+        while x != center_x:
+            print("loop checker")
+            y = math.ceil(intersept*(x-center_x)+senter_y)
+            if(img[x,y]==1):
+                ring_nums[line_index]+=1
+        line_index+=1
 
-    v_split = img.shape[0] // size #split num of vertical direction
-    h_split = img.shape[1] // size #split num of horizontal direction
+    NR = np.floor(np.mean(ring_nums))
 
-    i=0
-    j=0
-    for h_img in np.vsplit(img, v_split):
-        for v_img in np.hsplit(h_img, h_split):
-                #cv2.imwrite(r"C:\Users\user\Pictures\opencv_test_pic\test" + r"\img_%03.f"%(i) + "_%03.f"%(j) + ".jpg",v_img)
-                cv2.imwrite(r"C:\Users\sirim\Pictures\new\output\test_image" + r"\img_%03.f_%03.f"%(i,j) + ".tif",v_img)
-                j+=1 #update row index
-        j=0 #reset row index
-        i+=1 #update col index
-
-    return splited_imgs,splited_index
+    return NR
 
 #main function of this module
-def extractFeature(img,x_center,y_center,radius,model):
+def extractFeature(img,center_x,center_y,radius,model):
     #img:v channel of hsv
 
-    #select Low-noise line
+    #1.select Low-noise line
     #split img
     split_size = 64
-
+    splited_imgs,v_split,h_split = splitImg(img,split_size)
     
     #predict
     predictions = model.predict(splited_imgs)
     predict_results = predictions.argmax(axis=1) #result list of classification
 
     #create flag img
-    flag_img = numpy.zeros_like(img)
-    class_dict = {"high":0,"low":1}
-    
+    flag_img = createFlag(img,v_split,h_split,predict_results,split_size)
 
-    #obtain edge image
+    #get coordinate of outer wood
+    (outerX,outerY) = getCircleXY(radius,(center_x),(center_y),divn=20)
+
+    #extract good line
+    line_values = np.zeros_like(outerX)
+    line_index = 0
+    for outerx,outery in outerX,outerY:
+        intersept = (center_y - outery) / (center_x - outerx)
+        x = math.ceil(outerx)
+        while x != center_x:
+            print("loop checker")
+            y = math.ceil(intersept*(x-center_x)+senter_y)
+            if(flag_img[x,y]==1):
+                line_values[line_index]+=1
+        line_index+=1
+
+    #get good line indexes(prototype criteria)
+    median = np.median(line_values)
+    good_line_indexes = np.where(line_values >= median)[0]
+
+    #2.obtain edge image
     img_edge = obtainEdges(img)
 
-    #calculate NR
+    #3.calculate NR
+    NR = getNR(img_edge,center_x,center_y,good_outerX,good_outerY)
 
     #calculate AR
 
@@ -108,7 +139,10 @@ def extractFeature(img,x_center,y_center,radius,model):
 #test code for this module
 if __name__ == '__main__':
 
-    #load_img = cv2.imread(r"C:\Users\sirim\Pictures\new\DSC_0547_1_8.tif",cv2.IMREAD_GRAYSCALE)
+    load_img = cv2.imread(r"C:\Users\sirim\Pictures\new\deru.jpg",cv2.IMREAD_GRAYSCALE)
+
+    cv2.imshow('load_img',load_img)
+    cv2.waitKey(0)
 
     #img_blur = cv2.medianBlur(load_img,3)
 
@@ -121,9 +155,35 @@ if __name__ == '__main__':
     #cv2.imshow('edge',edge)
     #cv2.waitKey(0)
 
-    x,y = getCircleXY(2,(100,100))
-    print(x)
-    print(y)
+    #x,y = getCircleXY(2,(100,100))
+    #print(x)
+    #print(y)
+
+    model = tf.keras.models.load_model('test_model_73_rotrev.h5')
+    model.summary()
+
+    splited_imgs,v_split,h_split = splitImg(load_img)
+    splited_imgs = splited_imgs /255.0
+    splited_imgs = splited_imgs.reshape(-1,64,64,1)
+
+    predictions = model.predict(splited_imgs)
+    predict_results = predictions.argmax(axis=1) #result list of classification
+    #print(predict_results)
+
+    flag_img = createFlag(load_img,v_split,h_split,predict_results,split_size=64)
+
+    print(flag_img.shape)
+
+    line_values = np.array([10,11,12,13,14,15,16,17,18,19,20])
+    median = np.median(line_values)
+    good_line_indexes = np.where(line_values >= median)
+    print("median:{}".format(median))
+    print(good_line_indexes)
+    print(good_line_indexes[0])
+
+    #print(splited_imgs.shape)
+    #cv2.imshow('splited_imgs',splited_imgs[0])
+    #cv2.waitKey(0)
 
 
 
