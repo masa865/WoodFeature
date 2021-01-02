fe87e65ca3056571e82d15f8ba579de796a9b696
masa865 <sirimochimochi@gmail.com>
2021-01-02 17:43:21 +0900

    a

--- VSProjects/features_extraction/features_extraction/features_extraction.py
+++ VSProjects/features_extraction/features_extraction/features_extraction.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 #split image
-def splitImg(img,split_size=64):
+def splitImg(img,split_size=128):
 
     v_size = img.shape[0] // split_size * split_size
     h_size = img.shape[1] // split_size * split_size
@@ -34,12 +34,14 @@ def splitImg(img,split_size=64):
 def createFlag(img,v_split,h_split,predict_results,split_size=64):
 
     flag_img = np.zeros_like(img)
+    extractable_flag = 1
+
     k = 0
     for i in range(v_split):
         for j in range(h_split):
             if(predict_results[j+k]):
                 flag_img[i*split_size:i*split_size+split_size,
-                         j*split_size:j*split_size+split_size] = 1
+                         j*split_size:j*split_size+split_size] = extractable_flag
         k+=h_split
 
     return flag_img
@@ -49,11 +51,9 @@ def getCircleXY(radius,center_x,center_y):
 
     X_up = np.arange(center_x - radius, center_x + radius + 1)
     X = np.append(X_up,X_up[1:len(X_up)-1])
-    #print(X)
     Y_up = np.sqrt(radius**2 - (X_up-center_x)**2) + center_y
     Y_down = -Y_up + 2*center_y
     Y = np.append(Y_up,Y_down[1:len(Y_down)-1])
-    #print(Y)
 
     return (X,Y) #X,Y are numpy array
 
@@ -65,53 +65,69 @@ def obtainEdges(img,minVal=100,maxVal=200,filter_size=3):
     return img_edge
 
 def getNR(img,center_x,center_y,outerX,outerY):
-    #img is assumed edge image
+    #---------------------------------------------
+    #NR  :number of annual rings
+    #AR  :average of every ring(px)
+    #AC15:average width of 15th from the center(px)
+    #AO15: average width of 15th from the outside(px)
+    #img : assumed edge image
+    #ring_nums[line_index] : ring num of "line_index" th line
+    #---------------------------------------------
 
     ring_nums = np.zeros_like(outerX)
     line_index = 0
 
+    img_copy = np.copy(img)
 
     for outerx,outery in list(zip(outerX,outerY)):
-        i=0
-        intersept = (center_y - outery) / (center_x - outerx)
-        x = math.ceil(outerx)
-        same_line_flag = False
-        while x != center_x:
-            i+=1
-            print(i)
-            y = math.ceil(intersept*(x-center_x)+center_y)
-            if(img[x,y] != 0):
-                if(same_line_flag==False):
-                    ring_nums[line_index]+=1
-                    same_line_flag=True
-            else:
-                same_line_flag=False
-            
-            if(x>center_x):
-                x-=1
-            else:
-                x+=1
-        line_index+=1
 
-    NR = np.floor(np.mean(ring_nums))
+        if(center_x-outerx != 0): #intersept is not infinity
+            if (outerx > center_x):
+                X = np.arange(center_x, outerx+0.1,0.1)
+            if(outerx < center_x):
+                X = np.arange(outerx,center_x+0.1,0.1)
+
+            intersept = (center_y - outery) / (center_x - outerx)
+            Y = intersept*(X-center_x)+center_y
+            same_line_flag = False
+
+            if(abs(Y[0]-Y[1]) < 1.5): #distance between the points is not too large
+                for x,y in list(zip(X,Y)):
+                    img_copy[math.ceil(x),math.ceil(y)]=120
+                    if(img[math.ceil(x),math.ceil(y)] != 0):
+                        if(same_line_flag == False):
+                            ring_nums[line_index] += 1
+                            same_line_flag = True
+                    else:
+                        same_line_flag = False
+        line_index += 1
+
+    cv2.imshow("img_copy",img_copy)
+    cv2.waitKey(0)
+
+    NR = np.floor(ring_nums[np.nonzero(ring_nums)].mean())
+    print("-result--------")
+    print("ring_nums:")
     print(ring_nums)
-    print(NR)
+    print("NR(floor):{}".format(NR))
+    print("NR(float):{}".format(ring_nums[np.nonzero(ring_nums)].mean()))
     return NR
 
 #main function of this module
 def extractFeature(img,center_x,center_y,radius,model):
     #img:v channel of hsv
 
-    #1.select Low-noise line
-    #split img
-    split_size = 64
+    #----------------------------------------------------------------------------
+    #---1.extract Low-noise line--------------------------------------------------
+    #----------------------------------------------------------------------------
+    split_size = 128 #same size as model input
     splited_imgs,v_split,h_split = splitImg(img,split_size)
     
-    #predict
+    #prediction using model
     predictions = model.predict(splited_imgs)
     predict_results = predictions.argmax(axis=1) #result list of classification
 
-    #create flag img
+    #create flag image
     flag_img = createFlag(img,v_split,h_split,predict_results,split_size)
 
     #get coordinate of outer wood
@@ -121,23 +137,41 @@ def extractFeature(img,center_x,center_y,radius,model):
     line_values = np.zeros_like(outerX)
     line_index = 0
     for outerx,outery in outerX,outerY:
-        intersept = (center_y - outery) / (center_x - outerx)
-        x = math.ceil(outerx)
-        while x != center_x:
-            print("loop checker")
-            y = math.ceil(intersept*(x-center_x)+senter_y)
-            if(flag_img[x,y]==1):
-                line_values[line_index]+=1
-        line_index+=1
+
+        if(center_x-outerx != 0): #intersept is not infinity
+            if (outerx > center_x):
+                X = np.arange(center_x, outerx+0.1,0.1)
+            if(outerx < center_x):
+                X = np.arange(outerx,center_x+0.1,0.1)
+
+            intersept = (center_y - outery) / (center_x - outerx)
+            #print(intersept)
+            Y = intersept*(X-center_x)+center_y
+
+            if(abs(Y[0]-Y[1]) < 1.5): #distance between the points is not too large
+                for x,y in list(zip(X,Y)):
+                    if(img[math.ceil(x),math.ceil(y)] != 0):
+                        line_values[line_index] += 1
+        line_index += 1
 
     #get good line indexes(prototype criteria)
     median = np.median(line_values)
     good_line_indexes = np.where(line_values >= median)[0]
 
-    #2.obtain edge image
+    good_outerX = []
+    good_outerY = []
+    for index in good_line_indexes:
+        good_outerX.append(outerX[index])
+        good_outerY.append(outerY[index])
+
+    #----------------------------------------------------------------------------
+    #---2.obtain edge image------------------------------------------------------
+    #----------------------------------------------------------------------------
     img_edge = obtainEdges(img)
 
-    #3.calculate NR
+    #----------------------------------------------------------------------------
+    #---3.calculate NR-----------------------------------------------------------
+    #----------------------------------------------------------------------------
     NR = getNR(img_edge,center_x,center_y,good_outerX,good_outerY)
 
     #calculate AR
@@ -146,12 +180,6 @@ def extractFeature(img,center_x,center_y,radius,model):
 
     #calculate AO15
 
-    #---------------------------------------------
-    #NR  :number of annual rings
-    #AR  :average of every ring(cm)
-    #AC15:average width of 15th from the center(cm)
-    #AO15: average width of 15th from the outside(cm)
-    #---------------------------------------------
     return NR,AR,AC15,AO15
 
 def extractByTraditional(img,center_x,center_y,radius):
@@ -173,8 +201,6 @@ def extractByTraditional(img,center_x,center_y,radius):
     #2.calculate NR
     NR = getNR(img_edge,center_x,center_y,outerX[::10],outerY[::10])
 
-
-
     return NR,AR,AC15,AO15
 
 #test code for this module
@@ -182,33 +208,37 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     load_img = cv2.imread(r"C:\Users\sirim\Pictures\test_circle.png",0)
-    img_edge = obtainEdges(load_img)
 
-    #NR,AR,AC15,AO15=extractByTraditional(load_img,172,185,308-160)
-    center_x = 172
-    center_y = 185
-    (outerX,outerY) = getCircleXY(308-160,172,185)
-    plt.plot(outerX, outerY,marker='.',linestyle='None')
+    #img_edge = obtainEdges(load_img)
 
-    for outerx,outery in list(zip(outerX[::10],outerY[::10])):
-        if(center_x-outerx != 0):
-            if (outerx > center_x):
-                X = np.arange(center_x, outerx+0.1,0.1)
-            if(outerx < center_x):
-                X = np.arange(outerx,center_x+0.1,0.1)
+    #load_img,172,185,308-160
+    NR,AR,AC15,AO15=extractByTraditional(load_img,180,170,308-160)
 
-            intersept = (center_y - outery) / (center_x - outerx)
-            print(intersept)
-            Y = intersept*(X-center_x)+center_y
 
-            if(outery>center_y):np.clip(Y, None, outery)
-            else: np.clip(Y,outery, None)
+    #center_x = 172
+    #center_y = 185
+    #(outerX,outerY) = getCircleXY(308-160,172,185)
+    #plt.plot(outerX, outerY,marker='.',linestyle='None')
 
-            plt.plot(X, Y,marker='.',linestyle='None')
+    #for outerx,outery in list(zip(outerX[::10],outerY[::10])):
+    #    if(center_x-outerx != 0):
+    #        if (outerx > center_x):
+    #            X = np.arange(center_x, outerx+0.1,0.1)
+    #        if(outerx < center_x):
+    #            X = np.arange(outerx,center_x+0.1,0.1)
 
-    plt.axis("equal")
-    plt.grid(color="0.8")
-    plt.show() # 画面に表示
+    #        intersept = (center_y - outery) / (center_x - outerx)
+    #        print(intersept)
+    #        Y = intersept*(X-center_x)+center_y
 
-    cv2.imshow("result",img_edge)
-    cv2.waitKey(0)
+    #        if(outery>center_y):np.clip(Y, None, outery)
+    #        else: np.clip(Y,outery, None)
+
+    #        plt.plot(X, Y,marker='.',linestyle='None')
+
+    #plt.axis("equal")
+    #plt.grid(color="0.8")
+    #plt.show() # 画面に表示
+
+    #cv2.imshow("result",img_edge)
+    #cv2.waitKey(0)
